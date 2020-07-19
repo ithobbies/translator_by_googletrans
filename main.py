5b86d8ae3acfefa316852f85ae8cf9a05b45b278
ithobbies <dannyfeel93@gmail.com>
2020-07-19 12:53:09 +0300

    commit on 'master'

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from ui import Ui_MainWindow
 from googletrans import Translator
 
+class Lang():
+    def __init__(self):
+        self.selected_language = ''
 
+    def set_lang(self, selected_language):
+        self.selected_language = selected_language
+    
 
 class TranslateText(QtWidgets.QMainWindow):
     def __init__(self):
@@ -17,32 +23,20 @@ class TranslateText(QtWidgets.QMainWindow):
         self.ui.btn_translate.clicked.connect(self.translate)
         self.ui.btn_check_lang.setDown(True)
         
-        self.ui.btn_eng_lang.clicked.connect(self.translate_into_en)
-        self.ui.btn_ru_lang.clicked.connect(self.translate_into_ru)
-        self.ui.btn_ge_lang.clicked.connect(self.translate_into_de)
-    
-    def translate_into_en(self):
-        global selected_language
-        selected_language = 'en'
-    
-    def translate_into_ru(self):
-        global selected_language
-        selected_language = 'ru'
-
-    def translate_into_de(self):
-        global selected_language
-        selected_language = 'de'
+        self.ui.btn_eng_lang.clicked.connect(lambda: lang.set_lang('en'))
+        self.ui.btn_ru_lang.clicked.connect(lambda: lang.set_lang('ru'))
+        self.ui.btn_ge_lang.clicked.connect(lambda: lang.set_lang('de'))
 
     def translate(self):
-        global selected_language
         input_text = self.ui.input_text.toPlainText()
-        translated_text = translator.translate(input_text, dest=f'{selected_language}')        
+        translated_text = translator.translate(input_text, dest=f'{lang.selected_language}')        
         self.ui.output_text.setPlainText(translated_text.text)    
 
-selected_language = ''
-translator = Translator()      
-app = QtWidgets.QApplication([])
-application = TranslateText()
-application.show()
- 
-sys.exit(app.exec())
+if __name__ == '__main__':
+    lang = Lang()
+    translator = Translator()      
+    app = QtWidgets.QApplication([])
+    application = TranslateText()
+    application.show()
+    
+    sys.exit(app.exec())
